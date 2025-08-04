9079bd64749119ed91a28d1e92a1cf039862e3f7
Mouiz Majid <Mouiz Majid>
2025-08-04 17:58:42 -0400

    added code for the state of the site. when users enter text into the search bar, the movies match dynamically

--- reactsite/src/pages/Home.jsx
+++ reactsite/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import MovieCard from "../components/MovieCard"
 import { useState } from "react";
 function Home(){
+    const [searchQuery, setSearchQuery] = useState("");
 
-    
+
     const movies = [
         {id: 1, title: "Avengers", release_date: "2012"},
         {id: 2, title: "John Wick", release_date: "2014"},
@@ -11,8 +12,9 @@ function Home(){
         {id: 5, title: "Jurrasic Park", release_date: "1993"}
 
     ];
-    const handleSearch = () => {
-
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
     }
  
     return (
@@ -20,10 +22,19 @@ function Home(){
 
         <div className="home">
             <form onSubmit={handleSearch} className="search-form">
-                <input type ="text" placeholder="Search for movies..." className="search-input"/> 
-                <button type="submit" className="search-button">Search</button></form>
+                <input 
+                type ="text" 
+                placeholder="Search for movies..."
+                className="search-input" 
+                value= {searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}/> 
+                <button type="submit" className="search-button">Search</button>
+                
+                </form>
+            
             <div className="movies-grid">
                 {movies.map((movie) => (
+                    movie.title.toLowerCase().startsWith(searchQuery) &&
                     <MovieCard movie ={movie} key = {movie.id} /> 
                     ))}
             </div>
